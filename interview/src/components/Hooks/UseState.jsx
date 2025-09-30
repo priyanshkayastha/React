284cb22bd237a3d01f679abf60c567be4f87fe6d
Priyansh Kayastha <priyanshkayastha0803@gmail.com>
2025-09-30 19:26:09 +0530

    UseState

---
+++ interview/src/components/Hooks/UseState.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const UseState = () => {
+    const [count,setCount]=useState(0)
+    const handleClick=()=>{
+        setCount(count+1)
+    }
+  return <div>
+    <p>{count}</p>
+    <button onClick={handleClick}>Click me</button>
+  </div>
+  
+}
+
+export default UseState
